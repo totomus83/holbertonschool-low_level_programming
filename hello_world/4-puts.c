def75454be68d090c29674db91b4303d014f7639
totomus83 <tom.dibellonio@gmail.com>
2024-10-21 12:27:17 +0200

    forgot the backslash

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -5,8 +5,8 @@
  * Return: return 0 if success
 */
 
-int	main(void)
+int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
