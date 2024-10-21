eed2c73c0e048364a0238e41b4d5a8bfa7b00c65
totomus83 <tom.dibellonio@gmail.com>
2024-10-21 15:13:47 +0200

    test

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -14,9 +14,9 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n < 0)
-printf("is positive");
 if (n > 0)
+printf("is positive");
+if (n < 0)
 printf("is negative");
 if (n == 0)
 printf("is zero");
