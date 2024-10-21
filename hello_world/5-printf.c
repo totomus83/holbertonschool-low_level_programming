6025c4c170057ec6451772d444451cb468195c71
totomus83 <tom.dibellonio@gmail.com>
2024-10-21 12:36:42 +0200

    test

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: return 0 if success
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
