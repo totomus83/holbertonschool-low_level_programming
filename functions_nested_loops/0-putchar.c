7411288e997df19ebc1894680df0b81e3af0535a
totomus83 <tom.dibellonio@gmail.com>
2024-10-22 13:18:08 +0200

    test

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- entry point
+ * Return: return 0 if success
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	return (0);
+}
