1f9377ae93566609824eb57120fc3a466d1b5226
totomus83 <tom.dibellonio@gmail.com>
2024-10-22 15:38:13 +0200

    test

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,17 +1,19 @@
-#include <main.h>
+#include "main.h"
 
-
-
-void print alphabet(void)
+/**
+ * main - entry point
+ * print_alphabet - print the alphabet
+ * Return: return 0 if success
+ */
+void print_alphabet(void)
 {
 	char i;
 
-	i = a;
-	while ( i <= z)
+	i = 'a';
+	while (i <= 'z')
 	{
 		_putchar(i);
-		a++;
+		i++;
 	}
 	_putchar('\n');
-	return (0);
 }
