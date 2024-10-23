594ceb610060ab9faccb81003fd5e46b6381586c
totomus83 <tom.dibellonio@gmail.com>
2024-10-23 14:56:46 +0200

    test

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ *_islower - test if variable c is undercase
+ *@c: character to be verified
+ * Return: if succes return 1, if not return 0
+ */
+
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
