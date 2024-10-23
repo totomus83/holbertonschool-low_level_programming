1cc962e6054216b178e6039f12fb696951abd5f2
totomus83 <tom.dibellonio@gmail.com>
2024-10-23 16:23:15 +0200

    test

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_sign - test if variable c is positive, negative or 0
+ *@n: character to be verified
+ * Return: if succes return 1, if not return 0
+ */
+
+int print_sign(int n)
+{
+	if (n > '0')
+		_putchar('+');
+		return (1);
+	if (n == 0)
+		_putchar('0');
+		return (0);
+	if (n < 0)
+		_putchar('-');
+		return (-1);
+}
