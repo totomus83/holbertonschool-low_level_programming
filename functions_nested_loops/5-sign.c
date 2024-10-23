bf7dc9b779014fa89518e32a5fea908a0241cd19
totomus83 <tom.dibellonio@gmail.com>
2024-10-23 16:45:51 +0200

    test

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -9,12 +9,9 @@
 int print_sign(int n)
 {
 	if (n > '0')
-		_putchar('+');
 		return (1);
-	if (n == 0)
-		_putchar('0');
+	else if (n == 0)
 		return (0);
-	if (n < 0)
-		_putchar('-');
+	else
 		return (-1);
 }
