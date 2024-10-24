674640489fbe91494d52b0fdec6e4adb39a5a953
totomus83 <tom.dibellonio@gmail.com>
2024-10-24 11:53:58 +0200

    test

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - function that print the absolute value of an integer
+ * Return: return 0 if success
+ */
+int _abs(int i);
+{
+
+	if (i > 0)
+		return (i);
+	else
+		return (-i);
+}
