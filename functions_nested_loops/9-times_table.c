9b91b82c708019a877d6fd44c1e1976f93d1dc36
totomus83 <tom.dibellonio@gmail.com>
2024-10-25 13:39:11 +0200

    test

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * times_table - function printing the tables
+ *
+ */
+void times_table(void)
+{
+	int num;
+	int mul;
+
+	num = 0;
+	mul = 0;
+	while (num <= 9)
+	{
+		while (mul <= 9)
+		{
+			if ((num * mul) > 9)
+			{
+				_putchar(((num * mul) / 10) + '0');
+				_putchar(((num * mul) % 10) + '0');
+				if (mul < 9)
+					_putchar(',');
+			}
+			else if ((num * mul) < 10)
+			{
+				_putchar((num * mul) + '0');
+				if (mul < 9)
+					_putchar(',');
+				_putchar(' ');
+			}
+			_putchar(' ');
+			mul++;
+		}
+		num++;
+		_putchar('\n');
+		mul = 0;
+	}
+}
