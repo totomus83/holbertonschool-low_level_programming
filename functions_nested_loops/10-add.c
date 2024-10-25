4593b0f1a73a858dc7661a283cc7b16c7c617de8
totomus83 <tom.dibellonio@gmail.com>
2024-10-25 14:22:43 +0200

    test

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - function that add 2 variable to each other
+ *@a: 1st variable
+ *@b: 2nd variable
+ *Return: return the addition of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
