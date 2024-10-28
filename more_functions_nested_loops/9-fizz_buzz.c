2754ea39ab4ac460582c131bc4dc4c748870c688
totomus83 <tom.dibellonio@gmail.com>
2024-10-28 16:23:36 +0100

    test

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int i;
+
+	i = 1;
+	while (i < 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		if (i % 3 != 0 && i % 5 != 0)
+			printf("%d", i);
+		if (i % 3 == 0 && i % 5 != 0)
+			printf("Fizz");
+		if (i % 5 == 0 && i % 3 != 0)
+			printf("Buzz");
+		putchar(' ');
+		i++;
+	}
+	return (0);
+}
