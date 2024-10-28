bdcfebdc8f13a634a846a8e762217a60c65db5e6
totomus83 <tom.dibellonio@gmail.com>
2024-10-28 12:08:11 +0100

    fixed synthax

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,9 @@
 #include "main.h"
-
+/**
+ * _isupper - select only upper case letter
+ * Return: 0 if non upper case, 1 if uppercase
+ * @c: tested number
+ */
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
