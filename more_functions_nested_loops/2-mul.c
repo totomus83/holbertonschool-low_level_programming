8e9066aec7ed5bfcae1b3178dc3e35fbc8e39094
totomus83 <tom.dibellonio@gmail.com>
2024-10-28 12:17:44 +0100

    test

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - functions that multiply 2 integers
+ * @a: first value
+ * @b: second value
+ * Return: the results is returned
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -16,5 +16,6 @@ void print_to_98(int n);
 /* more functions */
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 
 #endif /* MAIN_H */
