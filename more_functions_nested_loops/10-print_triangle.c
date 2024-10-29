657105a6b65a4fdb732651d2b138cb305836b9e7
totomus83 <tom.dibellonio@gmail.com>
2024-10-29 15:30:48 +0100

    test

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_triangle - pting a triangle
+ * @size: determines the size
+ */
+
+void print_triangle(int size)
+{
+	int w;
+	int b;
+
+	b = 1;
+	w = 0;
+	if (size == 0)
+		_putchar('\n');
+	while (b <= size)
+	{
+		while (w <= size)
+		{
+			if ((size - b) >= w)
+				_putchar(' ');
+			else
+				_putchar('#');
+			w++;
+		}
+		b++;
+		w = 0;
+		_putchar('\n');
+	}
+}
