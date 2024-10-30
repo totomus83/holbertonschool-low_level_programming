b3458954e450d0bf1d026b3baaaf1d3b73a16cf9
totomus83 <tom.dibellonio@gmail.com>
2024-10-30 15:54:48 +0100

    test

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints an array
+ * @a: array
+ * @n: position in the array
+ */
+
+void print_array(int *a, int n)
+{
+	n = 0;
+	while (n < 5)
+	{
+		printf("%d", a[n]);
+		if (n < 4)
+			printf(", ");
+		n++;
+	}
+}
