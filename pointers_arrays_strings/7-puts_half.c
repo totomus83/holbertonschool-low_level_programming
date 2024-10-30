6410c2cab1dc82c7cd1758a46ad2ef722d39a478
totomus83 <tom.dibellonio@gmail.com>
2024-10-30 15:27:11 +0100

    test

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * puts_half - print the upper half of a string
+ * @str: string
+ */
+
+void puts_half(char *str)
+{
+	int l;
+	int i;
+
+	l = 0;
+	while (str[l] != '\0')
+	{
+		l++;
+	}
+
+	if (str[l] / 2 == 0)
+	{
+		i = l / 2;
+		while (i < l)
+		{
+			_putchar(str[i]);
+			i++;
+		}
+	}
+	else
+	{
+		i = l / 2 + 1;
+		while (i < l)
+		{
+			_putchar(str[i]);
+			i++;
+		}
+	}
+}
