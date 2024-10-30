c60363ae0648a2484da3fc601f358df9cf4a8dc9
totomus83 <tom.dibellonio@gmail.com>
2024-10-30 14:14:47 +0100

    test

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse the string s
+ * @s: string to be reversed
+ */
+void rev_string(char *s)
+{
+	int last;
+	int first;
+	int temp;
+
+	last = 0;
+	while (s[last] != '\0')
+	{
+		last++;
+	}
+	first = 0;
+	while (first < last / 2)
+	{
+		temp = s[first];
+		s[first] = s[last - 1 - first];
+		s[last - 1 - first] = temp;
+		first++;
+	}
+	_putchar('\n');
+}
