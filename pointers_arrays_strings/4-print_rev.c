a6677a4490ea5998209599ab10421286adf80504
totomus83 <tom.dibellonio@gmail.com>
2024-10-30 10:55:05 +0100

    test

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - print in reverse a string
+ * @s: string of characters
+ */
+void print_rev(char *s)
+{
+	int l;
+	int r;
+	int i;
+
+	l = 0;
+	while (s[l] != '\0')
+	{
+		l++;
+	}
+	i = l;
+	r = i - 1;
+	while (r > 0)
+	{
+		_putchar(s[r]);
+		r--;
+	}
+	_putchar('\n');
+}
