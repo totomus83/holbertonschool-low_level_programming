59b650ff90d656bf57ed70a9a097dc11bcf2797a
totomus83 <tom.dibellonio@gmail.com>
2024-11-03 23:44:04 +0100

    test

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -8,21 +8,21 @@
 
 char *leet(char *s)
 {
-	int i = 0;
-	int j;
-	char leet_chars[] = "43071";
-	char normal_chars[] = "aeotlAEOTL";
+    int i = 0;
+    int j;
+    char leet_chars[] = "43071";
+    char normal_chars[] = "aeotlAEOTL";
 
-	while (s[i] != '\0')
-	{
-		j = 0;
-		while (j < 10)
-		{
-			s[i] += (s[i] == normal_chars[j]) * (leet_chars[j / 2] - s[i]);
-			j++;
-		}
-		i++;
-	}
+    while (s[i] != '\0')
+    {
+        j = 0;
+        while (j < 10)
+        {
+            s[i] += (s[i] == normal_chars[j]) * (leet_chars[j / 2] - s[i]);
+            j++;
+        }
+        i++;
+    }
 
-	return (s);
+    return (s);
 }
