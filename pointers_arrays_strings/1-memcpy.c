ca871bd5b07c9d9fd38e503926db28664b1304b2
totomus83 <tom.dibellonio@gmail.com>
2024-11-04 15:16:57 +0100

    test

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
-/** _memcpy copies memory area
+/**
+ * _memcpy - copies memory area
  * @dest: destination
  * @src: memory to copy to destination
  * @n: memory left
+ * Return: dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
