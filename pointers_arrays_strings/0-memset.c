4aad131ac3f477a190df25f23c3b125870ede0fc
totomus83 <tom.dibellonio@gmail.com>
2024-11-04 11:09:57 +0100

    test

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,7 +1,11 @@
 #include "main.h"
 
-
-
+/**
+ * _memset - fills the first n bytes of the memory area pointed to by s with the constant byte b
+ * @s: value returned
+ * @b: constant
+ * @n: memory
+ */
 
 char *_memset(char *s, char b, unsigned int n)
 {
