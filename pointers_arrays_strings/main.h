927ed574d76277df6936804b21b3124221f5014e
totomus83 <tom.dibellonio@gmail.com>
2024-11-04 11:05:34 +0100

    test

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -46,4 +46,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+/*pointers project 3*/
+char *_memset(char *s, char b, unsigned int n);
+
 #endif /* MAIN_H */
