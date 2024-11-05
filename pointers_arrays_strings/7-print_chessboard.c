20eb5760b607bbdbcc0534c4a6a6a1f42d4ca813
totomus83 <tom.dibellonio@gmail.com>
2024-11-05 19:26:13 +0100

    test

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+	while (i < 8)
+	{
+		while (j < 8)
+		{
+			if (j == 7)
+			{
+				_putchar(a[i][j]);
+				_putchar('\n');
+			}
+			else
+				_putchar(a[i][j]);
+			j++;
+		}
+		i++;
+		j = 0;
+	}
+}
