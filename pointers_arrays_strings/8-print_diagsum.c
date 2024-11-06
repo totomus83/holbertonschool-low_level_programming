5841d66f5729793034373cb7fe55f5ebc93c7a0d
totomus83 <tom.dibellonio@gmail.com>
2024-11-06 15:57:55 +0100

    test

---
+++ pointers_arrays_strings/8-print_diagsum.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - print the sum of both diagonals of a square matrix
+ * @a: int pointer
+ * @size: int
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int d1;
+	int d2;
+
+	d1 = 0;
+	d2 = 0;
+	i = 0;
+	while (i < size)
+	{
+		d1 = d1 + a[(size + 1) * i];
+		d2 = d2 + a[(size - 1) * i];
+		i++;
+	}
+	printf("%d, %d\n", d1, d2);
+}
