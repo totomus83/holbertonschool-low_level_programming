cb6bd6bc2d053231dc9e17fbfc4b6924f5b79aea
totomus83 <tom.dibellonio@gmail.com>
2024-11-06 19:06:57 +0100

    fixed betty synthax and descrpition

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,15 +1,20 @@
 #include "main.h"
 
+/**
+ * _strlen_recursion - count the engths of a string
+ * @s: string
+ * Return: i when *s = '\0'
+ */
 
 int _strlen_recursion(char *s)
 {
 	int i;
 
 	i = 1;
-	if(*s)
+	if (*s)
 	{
 		i = i + _strlen_recursion(s + 1);
-		return(i);
+		return (i);
 	}
 	return (0);
 }
