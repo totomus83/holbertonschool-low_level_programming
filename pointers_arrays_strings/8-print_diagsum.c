3305767759ea32d0d1001f7de087a4f9deb26116
totomus83 <tom.dibellonio@gmail.com>
2024-11-06 16:08:05 +0100

    fixed

--- pointers_arrays_strings/8-print_diagsum.c
+++ pointers_arrays_strings/8-print_diagsum.c
@@ -8,17 +8,20 @@
 void print_diagsums(int *a, int size)
 {
 	int i;
+	int j;
 	int d1;
 	int d2;
 
 	d1 = 0;
 	d2 = 0;
 	i = 0;
+	j = 1;
 	while (i < size)
 	{
 		d1 = d1 + a[(size + 1) * i];
-		d2 = d2 + a[(size - 1) * i];
+		d2 = d2 + a[(size - 1) * j];
 		i++;
+		j++;
 	}
 	printf("%d, %d\n", d1, d2);
 }
