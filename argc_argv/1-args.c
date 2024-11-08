e16d5f28f3605f9abf078ea4dfb661ec13150365
totomus83 <tom.dibellonio@gmail.com>
2024-11-08 14:30:38 +0100

    test

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: count number of arguments
+ * @argv: array of arguments
+ * Return: return 0 if success
+ */
+
+int main(int argc, char __attribute__((unused)) **argv)
+{
+	printf("%d", argc);
+	return (0);
+}
