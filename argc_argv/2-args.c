b0101ccf2633e277a76edda615996e5e6c2ac994
totomus83 <tom.dibellonio@gmail.com>
2024-11-08 14:44:26 +0100

    test

---
+++ argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: nb of arguments
+ * @argv: array containing arg
+ * Return: 0 if success
+ */
+
+int main(int argc, char **argv)
+{
+	int i;
+
+	i = 0;
+	while (i < argc)
+	{
+		printf("%s", argv[i]);
+		if (i < argc - 1)
+			putchar('\n');
+		i++;
+	}
+	return (0);
+}
