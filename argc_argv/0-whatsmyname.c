9a43415d865b99dc5debb1fc6f11639da0b1ed1d
totomus83 <tom.dibellonio@gmail.com>
2024-11-08 14:08:26 +0100

    fixed synthax

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 
-int main (int argc, char **argv)
+/**
+ * main - return 0
+ * @argc: count arguments
+ * @argv: array containing arguments
+ * Return: 0 if success
+ */
+int main(int argc, char **argv)
 {
 	int i;
 
 	i = 0;
 	while (i < argc)
 	{
-		printf ("%s ", argv[i]);
+		printf("%s ", argv[i]);
 		i++;
 	}
 	return (0);
