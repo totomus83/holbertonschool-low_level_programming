d8236ff130b339f491a66fdce622d3c3edf236c4
totomus83 <tom.dibellonio@gmail.com>
2024-11-08 15:46:52 +0100

    test

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,10 +12,9 @@ int main(int argc, char **argv)
 {
 	if (argc < 3)
 	{
-		printf("Error");
+		printf("Error\n");
 		return (1);
 	}
-	else
-		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-return (0);
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
 }
