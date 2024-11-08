87e2735ab97e6531bf4f667d14918451048c1293
totomus83 <tom.dibellonio@gmail.com>
2024-11-08 15:38:29 +0100

    test

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: number of argument
+ * @argv: string
+ * Return: 1 if arguments are below 3, if else multiply them
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc < 3)
+	{
+		printf("Error");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
