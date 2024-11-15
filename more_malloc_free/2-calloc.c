5e5541d4bba8a6c58ac4ee070454d132de0cb56d
totomus83 <tom.dibellonio@gmail.com>
2024-11-15 15:22:48 +0100

    test

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,15 +1,20 @@
 #include "main.h"
 #include <stdlib.h>
 
-
+/**
+ * _calloc - allocate memory for an array
+ * @nmemb: adress to print
+ * @size: size of memory to allocate
+ * Return: return the adresses
+ */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *ret;
-	unsigned i, s;
+	unsigned int i, s;
 
 	i = 0;
-	if (nmemb == 0 ||size == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 	s = nmemb * size;
 	ret = malloc(s);
