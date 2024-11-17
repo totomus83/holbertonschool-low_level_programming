ac34d2baabbf8ebb397b8af0b912c40411e3da57
totomus83 <tom.dibellonio@gmail.com>
2024-11-17 18:49:58 +0100

    fixed?

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,20 @@
-struct dog
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - first sttruct
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner of the dog
+ *
+ * Desrpition: information about the dog
+ */
+
+
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_h;
+
+#endif
