241080595a720d8457c347dfd06899b565cb4e14
totomus83 <tom.dibellonio@gmail.com>
2024-11-20 23:09:57 +0100

    test

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,6 +1,10 @@
 #include "function_pointers.h"
 
-
+/**
+ * print_name - prints a name
+ * @name: name
+ * @f: function to print
+ **/
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
