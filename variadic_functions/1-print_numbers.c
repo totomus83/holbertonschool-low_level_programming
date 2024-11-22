1eb7bce8054326183c421ffea050a23fb1974436
totomus83 <tom.dibellonio@gmail.com>
2024-11-22 20:30:12 +0100

    test

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -12,22 +12,23 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list valist;
+	va_list args;
 	unsigned int i = 0;
 
 	if (n > 0)
 	{
-		va_start(valist, n);
+		va_start(args, n);
 
 		while (i < n)
 		{
-			printf("%d", va_arg(valist, int));
+			printf("%d", va_arg(args, int));
 
-			if (i != n - 1 && separator != NULL)
+			if (i != n - 1  && separator != NULL)
 				printf("%s", separator);
+
 			i++;
 		}
-		va_end(valist);
+		va_end(args);
 	}
 	printf("\n");
 }
