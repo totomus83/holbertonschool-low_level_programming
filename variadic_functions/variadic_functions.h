9aff7338fa621adaf338bba7aa15998a5aa3a41c
totomus83 <tom.dibellonio@gmail.com>
2024-11-22 20:44:53 +0100

    test

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -6,6 +6,8 @@ int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
 /**
  * struct format_types - Struct format_types
  * @identifier: The conversion specifier
