e577b4edc349a950cc306d22cac0443aa11bca9b
totomus83 <tom.dibellonio@gmail.com>
2024-11-22 20:14:40 +0100

    test

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -14,10 +14,10 @@ int sum_them_all(const unsigned int n, ...)
 	va_list valist;
 
 	result = 0;
-	va_start(valist, n);
 
 	if (n != 0)
 	{
+		va_start(valist, n);
 		for (i = 0; i < n; i++)
 		{
 			result += va_arg(valist, int);
