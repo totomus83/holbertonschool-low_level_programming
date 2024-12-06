37dbdb1472ae1fc61635e7a3660f90f755c15ff9
totomus83 <tom.dibellonio@gmail.com>
2024-12-06 18:59:42 +0100

    test

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef _LISTS_
-#define _LISTS_
+#ifndef LISTS_H_
+#define LISTS_H_
 
 #include <stddef.h>
 /**
@@ -22,4 +22,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+
 #endif
