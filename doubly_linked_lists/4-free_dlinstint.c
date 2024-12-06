38ab5c55f8c7b586d0a6da026002dc4cc79149eb
totomus83 <tom.dibellonio@gmail.com>
2024-12-06 20:22:59 +0100

    first draft

---
+++ doubly_linked_lists/4-free_dlinstint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdlib.h>
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+
+}
