6fa295e04782c8ac9560d4dd05f2db9105ab36a9
totomus83 <tom.dibellonio@gmail.com>
2024-12-08 21:40:51 +0100

    fixed error, forgot to set value of i

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -12,6 +12,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	int desc, i;
 	ssize_t bytes;
 
+	i = 0;
 	while (text_content != NULL && text_content[i] != '\0')
 		i++;
 	if (filename == NULL)
